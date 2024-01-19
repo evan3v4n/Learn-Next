b1ae8d85bcf8fcf0484e12b16d774e88e4ddd369
evan3v4n <evan.scott2050@Gmail.com>
2024-01-18 23:07:46 -0500

    built app

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CreateForm from './CreateForm'
+export default function page() {
+  return (
+    <main>
+        <h2 className='text-primary text-center'>Add a New Ticket</h2>
+        <CreateForm/>
+    </main>
+  )
+}
